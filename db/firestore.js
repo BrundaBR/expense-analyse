372a2a9d0b35e3b8d992c2f0f1be9167d1d97eed
BrundaBR <revadibrunda@gmail.com>
2026-02-05 07:06:22 +0000

    Removing cred

--- db/firestore.js
+++ db/firestore.js
@@ -1,14 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { ENV } from "./config.js";
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCYL24HMH00tTADjruB8P8RRIFwP-04aCY",
-  authDomain: "blog-69233.firebaseapp.com",
-  projectId: "blog-69233",
-  storageBucket: "blog-69233.appspot.com",
-  messagingSenderId: "835347766519",
-  appId: "1:835347766519:web:b789490db1c26be4ef47f2",
-  measurementId: "G-RVKRZDNKDM"
+  apiKey: ENV.API_KEY,
+  authDomain: ENV.AUTH_DOMAIN,
+  projectId: ENV.PROJECT_ID,
+  storageBucket: ENV.STORAGE_BUCKET,
+  messagingSenderId: ENV.MESSAGING_ID,
+  appId: ENV.APP_ID,
+  measurementId: ENV.MEASURMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
