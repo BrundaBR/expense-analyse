c3f049237beacf0d392936533a4301ad942b3dfb
BrundaBR <revadibrunda@gmail.com>
2026-02-17 08:12:44 +0000

    Add: Form and data push to db

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+import { storeData } from "../db/db_store.js";
+
 console.log("WELCOME TO JS");
 
 
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector(".expense-track");
+
+    form.addEventListener("submit", storeData);
+});
