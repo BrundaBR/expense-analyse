21f706fddc6bc246473bda1b505f1a4edd47d38e
BrundaBR <revadibrunda@gmail.com>
2026-03-24 01:30:15 +0000

    Added dynamic rows

--- js/index.js
+++ js/index.js
@@ -6,4 +6,23 @@ console.log("WELCOME TO JS");
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.querySelector(".expense-track");
     form.addEventListener("submit", storeData);
+     const addBtn = document.querySelector(".add-row");
+    addBtn.addEventListener("click", addRow);
 });
+
+function addRow(event){
+    event.preventDefault();
+    const table = document.getElementById("expense-table-body");
+    console.log("TABLE:", table); // 👈 ADD THIS
+
+    const newRow = document.createElement("tr");
+    newRow.innerHTML = `
+        <td><input type="text" name="category"></td>
+        <td><input type="number" name="amount"></td>
+        <td><input type="text" name="comment"></td>
+        <td><input type="date" name="date"></td>
+    `;
+
+    table.appendChild(newRow);
+
+}
